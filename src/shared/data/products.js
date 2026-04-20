f6501e97f940e27cded40b67121aed8289a585c9
Facundo Robert <robert.facundo@hotmail.com>
2026-04-20 11:50:58 -0300

    favicon title

---
+++ src/shared/data/products.js
@@ -0,0 +1,82 @@
+export const products = [
+  {
+    id: "product-1",
+    brand: "Off-White",
+    name: `Out Of Office "Ooo" sneakers`,
+    description: "Energize your look with a fresh take on heritage adidas style. The adidas Daily 3.0 Shoes cut a classic profile with a modern suede upper. Your walk across campus or commute across town has never looked or felt this good.",
+    benefits: `Regular fit
+              Lace closure
+              Rubber outsole with vulcanized look
+              Imported`,
+    price: 775,
+    images: {
+      main: "/assets/images/products/off-white-1.png",
+      gallery: [
+        "/assets/images/products/off-white-1.png",
+        "/assets/images/products/off-white-2.png",
+        "/assets/images/products/off-white-3.png",
+        "/assets/images/products/off-white-4.png",
+      ],
+    },
+  },
+  {
+    id: "product-2",
+    brand: "Nike",
+    name: `Nike Gamma Force`,
+    description: "Energize your look with a fresh take on heritage adidas style. The adidas Daily 3.0 Shoes cut a classic profile with a modern suede upper. Your walk across campus or commute across town has never looked or felt this good.",
+    benefits: `Regular fit
+              Lace closure
+              Rubber outsole with vulcanized look
+              Imported`,
+    price: 200,
+    images: {
+      main: "/assets/images/products/adidas-1.png",
+      gallery: [
+        "/assets/images/products/nike-force-1.png",
+        "/assets/images/products/nike-force-2.png",
+        "/assets/images/products/nike-force-3.png",
+        "/assets/images/products/nike-force-4.png",
+      ],
+    },
+  },
+  {
+    id: "product-3",
+    brand: "Nike",
+    name: `Cosmic Unity 3`,
+    description: "Energize your look with a fresh take on heritage adidas style. The adidas Daily 3.0 Shoes cut a classic profile with a modern suede upper. Your walk across campus or commute across town has never looked or felt this good.",
+    benefits: `Regular fit
+              Lace closure
+              Rubber outsole with vulcanized look
+              Imported`,
+    price: 160,
+    images: {
+      main: "/assets/images/products/adidas-1.png",
+      gallery: [
+        "/assets/images/products/nike-cosmic-1.png",
+        "/assets/images/products/nike-cosmic-2.png",
+        "/assets/images/products/nike-cosmic-3.png",
+        "/assets/images/products/nike-cosmic-4.png",
+      ],
+    },
+  },
+   {
+    id: "product-4",
+    brand: "Adidas",
+    name: `DAILY 3.0 SHOES`,
+    description: "Energize your look with a fresh take on heritage adidas style. The adidas Daily 3.0 Shoes cut a classic profile with a modern suede upper. Your walk across campus or commute across town has never looked or felt this good.",
+    benefits: `Regular fit
+              Lace closure
+              Rubber outsole with vulcanized look
+              Imported`,
+    price: 160,
+    images: {
+      main: "/assets/images/products/adidas-1.png",
+      gallery: [
+        "/assets/images/products/adidas-1.png",
+        "/assets/images/products/adidas-2.png",
+        "/assets/images/products/adidas-3.png",
+        "/assets/images/products/adidas-4.png",
+      ],
+    },
+  },
+]
